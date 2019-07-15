9f6efb59842860f7c5c379cd6892d0be02446f17
marumuji <marumuji@gmail.com>
2019-07-15 22:45:55 +0900

    fix error of initial access

--- kondate/views.py
+++ kondate/views.py
@@ -12,7 +12,7 @@ def get_menus():
     mains = db.session.query(Menu).filter(Menu.type=="main")
     subs = db.session.query(Menu).filter(Menu.type=="sub")
     soups = db.session.query(Menu).filter(Menu.type=="soup")
-    
+
     main = mains[random.randrange(0, mains.count())]
     sub = subs[random.randrange(0, subs.count())]
     soup = soups[random.randrange(0, soups.count())]
