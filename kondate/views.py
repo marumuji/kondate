fe3391665022f1b67a496d5431102f219f9168a8
marumuji <marumuji@gmail.com>
2019-08-12 12:49:08 +0900

    メニュー変更ボタン追加

--- kondate/views.py
+++ kondate/views.py
@@ -17,6 +17,34 @@ def get_menus():
     sub = subs[random.randrange(0, subs.count())]
     soup = soups[random.randrange(0, soups.count())]
 
-    return render_template('show_menus.html', main=main, sub=sub, soup=soup)
+    return render_template('show_menus.html', main=main.name, sub=sub.name, soup=soup.name)
 
-import random
+@app.route('/main', methods=['GET'])
+def change_main():
+    mains = db.session.query(Menu).filter(Menu.type=="main")
+    main = mains[random.randrange(0, mains.count())]
+
+    sub = request.args.get('sub', '')
+    soup = request.args.get('soup', '')
+
+    return render_template('show_menus.html', main=main.name, sub=sub, soup=soup)
+
+@app.route('/sub', methods=['GET'])
+def change_sub():
+    subs = db.session.query(Menu).filter(Menu.type=="sub")
+    sub = subs[random.randrange(0, subs.count())]
+
+    main = request.args.get('main', '')
+    soup = request.args.get('soup', '')
+
+    return render_template('show_menus.html', main=main, sub=sub.name, soup=soup)
+
+@app.route('/soup', methods=['GET'])
+def change_soup():
+    soups = db.session.query(Menu).filter(Menu.type=="soup")
+    soup = soups[random.randrange(0, soups.count())]
+    main = request.args.get('main', '')
+    sub = request.args.get('sub', '')
+
+    return render_template('show_menus.html', main=main, sub=sub, soup=soup.name)
+
